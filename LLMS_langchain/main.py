c6db79bfae9125818331ce5d5fc0ce79e0c8a4fb
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-08-29 10:50:39 +0530

    working project with langchan

---
+++ LLMS_langchain/main.py
@@ -0,0 +1,21 @@
+import os
+from constants import openai_key
+from langchain.llms import OpenAI
+
+import streamlit as st
+
+os.environ['OPENAI_API_KEY']=openai_key
+
+# streamlit framework
+
+st.title('langchain With OpenAI API')
+input_text=st.text_input('search the you want')
+
+
+
+# OPENAI LLMS
+llm = OpenAI(temperature=0.3)
+
+if input_text:
+    st.write(llm(input_text))
+
